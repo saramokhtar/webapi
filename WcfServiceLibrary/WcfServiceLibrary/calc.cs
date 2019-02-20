8d7c6048b1c269fd30debba813a7d678ff14a2f3
sara mokhtar <smokhtar362@gmail.com>
2019-02-20 20:44:21 +0200

    frist

---
+++ WcfServiceLibrary/WcfServiceLibrary/calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary
+{
+    class calc : icalc
+    {
+        List<string> names = new List<string>() {"samy","hany","fady","ezaat"};
+        public List<string> add(string name)
+        {
+            names.Add(name);
+            return names;
+        }
+
+        public List<string> getall()
+        {
+            return names;
+        }
+
+        public string getbyindex(string index)
+        {
+            int _index = int.Parse(index);
+            return names[_index];
+        }
+    }
+}
---
+++ WcfServiceLibrary/WcfServiceLibrary/icalc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WcfServiceLibrary
+{
+    [ServiceContract]
+    interface icalc
+    {
+        [WebInvoke (Method ="PUT" , UriTemplate ="addname")]
+        [OperationContract]
+        List<string> add(string name);
+        [WebInvoke(Method = "GET", UriTemplate = "getall")]
+        [OperationContract]
+        List<string> getall();
+        [WebInvoke(Method = "GET", UriTemplate = "getbyindex/{index}")]
+        [OperationContract]
+        string getbyindex(string index);
+    }
+}
